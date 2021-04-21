b168b2a2c5bc243ce7a978e38f387fe811e32ab2
Alfonso Torres <atorresjr1@hotmail.com>
2021-04-20 23:13:10 -0500

    Added Notes Design

---
+++ src/components/NoteAdd.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-yellow-300 text-yellow-700 rounded space-y-2 flex justify-center items-center">
+        <button class="opacity-40 transform transition-all hover:scale-105 hover:opacity-60">
+            <ic:baseline-add-to-photos class="w-40 h-40 40" />
+        </button>
+    </div>
+</template>
+  
