3e3050f3194abef4f006cb524903d9a700c77d9e
Alfonso Torres <atorresjr1@hotmail.com>
2021-04-20 23:41:39 -0500

    Added Form

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import vue from '@vitejs/plugin-vue'
 import ViteComponents from 'vite-plugin-components'
 import Icons, { ViteIconsResolver } from 'vite-plugin-icons'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': 'src',
+      '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
   plugins: [
