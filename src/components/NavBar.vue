375fbe11bbf93e32919cab6cf1e7d46ae38578d2
Alfonso Torres <atorresjr1@hotmail.com>
2021-04-20 22:28:17 -0500

    Added NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-blue-700 p-4">
+        <div class="flex place-items-center space-x-2 text-coolGray-200 text-3xl container mx-auto">
+            <mdi-account />
+            <p class="font-thin tracking-wider">
+                Notes
+                <span class="font-bold">App</span>
+            </p>
+        </div>
+    </div>
+</template>
