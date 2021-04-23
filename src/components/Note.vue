e9a8e7a1216fcf14103d2dfa31c800637dfa3212
Alfonso Torres <atorresjr1@hotmail.com>
2021-04-22 22:14:26 -0500

    Added API Connection

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,19 +1,51 @@
 <template>
-    <div class="bg-yellow-300 text-yellow-800 rounded text-center p-2 space-y-2">
-        <h3 class="text-2xl font-thin tracking-wide">Note Title</h3>
-        <p class="text-sm font-medium text-yellow-900">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sapiente
-            architecto perspiciatis laudantium, quasi quam error quo veniam, odit
-            voluptatibus explicabo. Dolores cumque atque voluptate! Rem ea vel illo
-            laborum?
-        </p>
-        <div class="flex justify-end space-x-5">
-            <button class="hover:text-green-800">
+    <div class="bg-yellow-400 text-yellow-800 rounded text-center p-2 space-y-2">
+        <h3 class="text-2xl font-thin tracking-wide">{{ $props.note.title }}</h3>
+        <p class="text-sm font-medium text-yellow-900">{{ $props.note.content }}</p>
+        <div class="flex justify-end space-x-4">
+            <button @click="editNote(note)" class="hover:text-green-800">
                 <mdi:file-document-edit />
             </button>
-            <button class="hover:text-red-800">
+            <button @click="removeNote($props.note.id)" class="hover:text-red-800">
                 <flat-ui:trash />
             </button>
         </div>
     </div>
 </template>
+
+<script setup>
+import { defineProps, defineEmit } from 'vue'
+import { remove, showToggle, noteToEdit } from '~/helpers/useNotes'
+
+defineProps({
+    note: Object,
+    default: {
+        id: 0,
+        title: '',
+        content: '',
+    },
+})
+
+const emit = defineEmit(['deleted'])
+
+const removeNote = async id => {
+    await remove(id)
+    emit('deleted')
+}
+
+const editNote = note => {
+    noteToEdit.value = note
+    showToggle()
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+ 
